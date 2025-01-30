2db96bd680c661c115dcb1c2b174f4f80fcd203c
Ashley Trinh <atrnh@users.noreply.github.com>
2025-01-29 18:34:52 -0800

    temporarly add example/ directory so I can test stuff

---
+++ example/format.js
@@ -0,0 +1,6 @@
+window.storyFormat({
+  "name": "Format name",
+  "version": "0.0.0",
+  "description": "Format description",
+  "source": "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n  <meta charset=\"UTF-8\">\n  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n  <title>{{STORY_NAME}}</title>\n</head>\n<body>\n  {{STORY_DATA}}\n  <script>\n    !function(){\"use strict\";console.log(\"hello\")}();\n  </script>\n</body>\n</html>"
+});
